df98444c56f66e984efa5f02325b60139bcc7f4e
rajinders-sapient <rsingh120@sapient.com>
2016-02-23 17:12:35 +0530

    Initial commit

---
+++ app/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://<user>:<pass>@apollo.modulusmongo.net:27017/i2gypIdu' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
